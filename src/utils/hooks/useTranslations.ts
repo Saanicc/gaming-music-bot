fce9dc1b9d403f422dad1de7c3607b2054043685
Saanicc <mattias470@gmail.com>
2026-02-25 02:13:20 +0100

    Split up t and useTranslation function

--- src/utils/hooks/useTranslations.ts
+++ src/utils/hooks/useTranslations.ts
@@ -2,26 +2,30 @@ import {
   DEFAULT_LANGUAGE,
   locales,
   getBotLanguageFromCache,
+  LanguageCode,
 } from "@/ui/translations";
+
+const t = (
+  lang: LanguageCode,
+  key: string,
+  vars?: Record<string, string>
+): string => {
+  const translations = locales[lang] ?? locales[DEFAULT_LANGUAGE];
+  const value =
+    key.split(".").reduce((obj: any, k) => obj?.[k], translations) ??
+    key
+      .split(".")
+      .reduce((obj: any, k) => obj?.[k], locales[DEFAULT_LANGUAGE]) ??
+    key;
+
+  if (!vars) return value;
+  return Object.entries(vars).reduce(
+    (str, [k, v]) => str.replace(`{{${k}}}`, v),
+    value
+  );
+};
 
 export function useTranslations(guildId: string) {
   const lang = getBotLanguageFromCache(guildId) ?? DEFAULT_LANGUAGE;
-
-  console.log("Language from cache", lang);
-
-  return function t(key: string, vars?: Record<string, string>): string {
-    const translations = locales[lang] ?? locales[DEFAULT_LANGUAGE];
-    const value =
-      key.split(".").reduce((obj: any, k) => obj?.[k], translations) ??
-      key
-        .split(".")
-        .reduce((obj: any, k) => obj?.[k], locales[DEFAULT_LANGUAGE]) ??
-      key;
-
-    if (!vars) return value;
-    return Object.entries(vars).reduce(
-      (str, [k, v]) => str.replace(`{{${k}}}`, v),
-      value
-    );
-  };
+  return (key: string, vars?: Record<string, string>) => t(lang, key, vars);
 }
