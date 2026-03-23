00b12491b22a3f199f24fe6ec6f010339cebd191
Saanicc <mattias470@gmail.com>
2026-03-24 00:17:26 +0100

    Add docker scripts file (#32)
    * Add docker scripts file
    
    * Update node engine version

---
+++ docker-scripts.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { execSync } from "child_process";
+import packageJson from "./package.json" with { type: "json" };
+
+const version = packageJson.version;
+const command = process.argv[2];
+
+const cmds = {
+  build: `docker build --platform linux/amd64 -t gaming-music-bot:${version} -t gaming-music-bot:latest .`,
+  save: `docker save gaming-music-bot:${version} -o music-discord-bot-${version}.tar`,
+};
+
+if (!cmds[command]) {
+  console.error(`Unknown command: ${command}. Use 'build' or 'save'.`);
+  process.exit(1);
+}
+
+console.log(`Running: ${cmds[command]}`);
+execSync(cmds[command], { stdio: "inherit" });
